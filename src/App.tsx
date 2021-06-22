365525f235025f8b538e45a2a506696971b4d932
Caio Ferrarezi <caioferrarezi2@gmail.com>
2021-06-21 21:22:12 -0300

    :construction: wip(default) adiciona layout para home e nova sala

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import { Home } from './pages/Home'
+import { NewRoom } from './pages/NewRoom';
+
 function App() {
   return (
-    <h1>Hello World!</h1>
+    <BrowserRouter>
+      <Route path="/" exact component={Home} />
+      <Route path="/salas/nova" component={NewRoom} />
+    </BrowserRouter>
   );
 }
 
